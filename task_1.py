94e11b906571feac64d454357ed12aad9cc41783
mkh888 <55121264+mkh888@users.noreply.github.com>
2019-09-19 18:39:40 +0530

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,18 @@
+import collections
+
+input_list=[3,5,[3,56],3,'guvi',[3,7,'zen',4.6],5,2,'pit']
+max=0
+print(len(input_list))
+sub_list=[]
+for i in input_list:
+	if isinstance(i, collections.Iterable) and not isinstance(i, (str)):
+		sub_list.append(i)
+max_list=[]
+for i in sub_list:
+	if max<len(i):
+		max=len(i)
+		max_list=i
+print(max_list,len(max_list))			
+print(input_list.index(max_list))	
+print(max_list[0])			
+
